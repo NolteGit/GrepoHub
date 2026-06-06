42b0a2201ed237a0ba432ac386eabf88e7eeb67d
Henrik Funke <funkehenrik@gmail.com>
2026-06-06 18:18:25 +0200

    layout improvs

--- src/app/pages/planner/components/planner-mode-switch/planner-mode-switch.ts
+++ src/app/pages/planner/components/planner-mode-switch/planner-mode-switch.ts
@@ -28,7 +28,6 @@ export class PlannerModeSwitch {
   readonly activeMode = input.required<PlannerMode>();
   readonly cityDetailsVisible = input(false);
   readonly troopDetailsVisible = input(false);
-  readonly compactDetailsLabels = input(false);
   readonly modeSelected = output<PlannerMode>();
   readonly detailsToggled = output<PlannerDetailsToggle>();
 
